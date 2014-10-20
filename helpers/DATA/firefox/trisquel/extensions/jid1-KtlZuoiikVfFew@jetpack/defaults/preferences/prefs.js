14e6d0ed4be07a4d6bf94a141af83b7d60f1ac5f
Ruben Rodriguez <ruben@gnu.org>
2014-10-20 02:43:14 +0200

    Make generation scripts distro-independent

--- helpers/DATA/firefox/trisquel/extensions/jid1-KtlZuoiikVfFew@jetpack/defaults/preferences/prefs.js
+++
@@ -1,3 +0,0 @@
-pref("extensions.jid1-KtlZuoiikVfFew@jetpack.whitelist", "");
-pref("extensions.jid1-KtlZuoiikVfFew@jetpack.complaint_tab", true);
-pref("extensions.jid1-KtlZuoiikVfFew@jetpack.display_notifications", false);
