c4936caa471bf2105ce977fe9b3bc0acf74be9e5
Ruben Rodriguez <ruben@gnu.org>
2017-04-15 00:10:32 -0400

    Branding files updated for v52

--- data/branding/icecat/pref/icecat-branding.js
+++ data/branding/icecat/pref/icecat-branding.js
@@ -27,3 +27,6 @@ pref("browser.search.param.yahoo-fr-cjkt", ""); // now unused
 pref("browser.search.param.yahoo-fr-ja", "");
 pref("browser.search.param.yahoo-f-CN", "");
 
+// Number of usages of the web console or scratchpad.
+// If this is less than 5, then pasting code into the web console or scratchpad is disabled
+pref("devtools.selfxss.count", 0);
