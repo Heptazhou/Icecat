95d8edd62741b0ef504f0c47e374c8af90650458
Ruben Rodriguez <ruben@gnu.org>
2018-02-25 20:10:20 -0500

    Version bumped to v52.3.0ESR, plus tuned up the buildscripts

--- data/buildscripts/buildall.sh
+++ data/buildscripts/buildall.sh
@@ -22,9 +22,15 @@ export CCACHE_DIR=$HOME/ccache/$1-$3
 mkdir $SRCDIR/../../toolchains || true
 cd $SRCDIR/../../toolchains
 sh ../data/buildscripts/toolchain-${1}.sh
-cd $SRCDIR/python/compare-locales
+
+apt-get install -y --force-yes mercurial
+cd /usr/local/src
+hg clone http://hg.mozilla.org/build/compare-locales/
+cd compare-locales/
+hg checkout FIREFOX_45_2_0esr_RELEASE
 python setup.py install
 cp /usr/local/bin/compare* /usr/bin
+
 cd $SRCDIR
 sh ../../data/buildscripts/build-${1}.sh
 EOF
@@ -32,18 +38,19 @@ EOF
 env -i TERM=screen eatmydata sudo HOME=/home/ruben BUILDDIST=$2 ARCH=$3 pbuilder execute $SRCDIR/run.sh
 }
 
-buildpackage windows belenos amd64 |tee  windows.log 2>&1
-buildpackage mac belenos amd64 |tee mac.log 2>&1
+#buildpackage windows belenos amd64 |tee  windows.log 2>&1
+#buildpackage mac belenos amd64 |tee mac.log 2>&1
 buildpackage gnulinux belenos i386 |tee gnulinux-i386.log 2>&1
-mv $SRCDIR/obj-gnulinux $SRCDIR/obj-gnulinux-i386
+sudo mv $SRCDIR/obj-gnulinux $SRCDIR/obj-gnulinux-i386
 buildpackage gnulinux belenos amd64 |tee gnulinux-amd64.log 2>&1
-mv $SRCDIR/obj-gnulinux $SRCDIR/obj-gnulinux-amd64
-buildpackage android belenos i386  |tee android.log 2>&1
+sudo mv $SRCDIR/obj-gnulinux $SRCDIR/obj-gnulinux-amd64
+#buildpackage android belenos amd64  |tee android.log 2>&1
 
 rm binaries -rf
 mkdir binaries/langpacks -p
-cp $1/obj-windows/dist/icecat*.zip binaries
-cp $1/obj-mac/dist/icecat/icecat*.dmg binaries
+#cp $1/obj-windows/dist/icecat*.zip binaries
+#cp $1/obj-mac/dist/icecat/icecat*.dmg binaries
 cp $1/obj-gnulinux*/dist/icecat*.bz2 binaries
-cp $1/obj-android/dist/icecat*.apk binaries
+#cp $1/obj-android/dist/icecat*.apk binaries
 cp $1/obj-gnulinux-amd64/dist/linux-x86_64/xpi/* binaries/langpacks
+rename 's/linux/gnulinux/' binaries/*
