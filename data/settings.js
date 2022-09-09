0eca6f525b0ea79180fda7c25232cd873c681024
Ruben Rodriguez <ruben@trisquel.info>
2022-09-08 20:57:37 -0400

    Enable onion dns resolving

--- data/settings.js
+++ data/settings.js
@@ -402,3 +402,7 @@ pref("browser.contentblocking.report.vpn-promo.url", "");
 pref("browser.contentblocking.report.vpn-android.url", "");
 pref("browser.contentblocking.report.vpn-ios.url", "");
 pref("browser.privatebrowsing.promoEnabled", false);
+
+// Enable onion dns resolving
+pref("network.dns.blockDotOnion", false);
+pref("network.http.referer.hideOnionSource", true);
